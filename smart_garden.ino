7b5fff99d93fdf791a81ddcbc21401d7005b0a98
MinhLe <82821231+minh071199@users.noreply.github.com>
2021-09-09 21:33:35 +0700

    final_version
    + Thêm chức năng cho cảm biến ánh sáng và cảm biến mực nước.
    + Tối ưu code ngắn gọn hơn.

--- smart_garden.ino
+++ smart_garden.ino
@@ -1,85 +1,92 @@
-#define BLYNK_PRINT Serial
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 #include <DHT.h>
-#include "DHT.h"
 
+#define BLYNK_PRINT Serial
 #define DHTPIN 2
-#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT11
+#define waterLevelSensor 32
+#define moistureLevelSensor 34
+#define lightSensor 35
+#define MOTOR_EN 4
+#define LIGHT_EN 5
 
-//const int waterLevelSensor = 2;
-const int moistureLevelSensor = 34;
-//const int relay = 23;
-
-//int waterLevel;
-int moistureLevel;
-int MOTOR_EN = 4;
-int motor = 0;
+float waterLevel = 0;
+float moistureLevel = 0;
+float light_level = 0;
+float t = 0;
+float h = 0;
+bool motor = 0;
+bool light = 0;
 
 DHT dht(DHTPIN, DHTTYPE);
 BlynkTimer timer;
 
-// Go to the Project Settings (nut icon).
-char auth[] = "mRcpBmizT-8Uz6ie33nXe9mYQpzD_1nP";
-
+char auth[] = "5YStVOxN_2ANlCBwm7TOwI7iwuBosYuI";
 char ssid[] = "Ngoi Nha Hoa Hong";
 char pass[] = "243tamtrinh";
 
-
-void sendSensor1()
+void sendSensor1() //DHT11
 {
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
-    
+  h = dht.readHumidity();
+  t = dht.readTemperature();
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
   Blynk.virtualWrite(V5, h);
   Blynk.virtualWrite(V6, t);
-
 }
 
-void sendSensor2()
+void sendSensor2() //Do am dat
 {
-//  waterLevel = analogRead(waterLevelSensor);
-//  waterLevel = map(waterLevel,0,2000,0,20);
-//  moistureLevel = analogRead(moistureLevelSensor);
-//  moistureLevel = map(moistureLevel,0,4000,0,20);
-  moistureLevel = 100 - map(analogRead(moistureLevelSensor), 0, 5000, 0, 100);
-  
-
-//  Blynk.virtualWrite(V7, waterLevel);
+  moistureLevel = 100 - map(analogRead(moistureLevelSensor), 0, 4095, 0, 100);
   Blynk.virtualWrite(V7, moistureLevel);
 }
 
+void sendSensor3() //Light sensor
+{
+  light_level = map(analogRead(lightSensor), 0, 4095, 0, 100);
+  Blynk.virtualWrite(V8, light_level);
+}
 
+void sendSensor4() //Water Level sensor
+{
+  waterLevel = map(analogRead(waterLevelSensor), 0, 4095, 0, 100);
+  Blynk.virtualWrite(V9, waterLevel);
+}
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
   pinMode(MOTOR_EN, OUTPUT);
-  digitalWrite(MOTOR_EN, LOW);
+  digitalWrite(MOTOR_EN, HIGH);
+  pinMode(LIGHT_EN, OUTPUT);
+  digitalWrite(LIGHT_EN, HIGH);
   dht.begin();
-
-    // Setup a function to be called every second
-  timer.setInterval(2000L, sendSensor1);
-  timer.setInterval(1000L, sendSensor2);
-
+  timer.setInterval(2000L, sendSensor1); //DHT11 send
+  timer.setInterval(5000L, sendSensor2); //Do Am Dat
+  timer.setInterval(1000L, sendSensor3); //LightSensor
+  timer.setInterval(1000L, sendSensor4); //WaterLevel
 }
-
 
 void loop() {
   Blynk.run();
   timer.run();
-  if(moistureLevel<30)
-  motor = 1;
-  else motor = 0;
   
+  if(moistureLevel<=70)
+  motor = 1;
+  else if (moistureLevel>=90)
+  motor = 0;
   digitalWrite(MOTOR_EN, motor);
-  delay(300);
+  
+  if(waterLevel>=33)
+  motor = 0;
+  digitalWrite(MOTOR_EN, motor);
+  
+  if(light_level<=30)
+  light = 1;
+  else light = 0;
+  digitalWrite(LIGHT_EN, light);
 }
